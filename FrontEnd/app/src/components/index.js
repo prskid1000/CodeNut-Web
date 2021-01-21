849a619a1a0a4a62123c5f12272eb4b948aad4ec
Prithwiraj Samanta <prskid1000@gmail.com>
2021-01-21 20:04:57 +0530

    Code Added

--- FrontEnd/app/src/components/index.js
+++ FrontEnd/app/src/components/index.js
@@ -8,7 +8,10 @@ class Index extends React.Component
         super(props);
         this.state =
         {
-          user:"prskid1000"
+          alert:"Welcome to site",
+          user:"prskid1000",
+          posts: [{ 'question': 'WHat is OOPs?', 'desciption': 'Descibe it! Please give defination also.' }, { 'question': 'WHat is Java?', 'desciption': 'Descibe it! Please give defination also.' }, { 'question': 'WHat is Java?', 'desciption': 'Descibe it! Please give defination also.' }],
+          contributors: [{ 'userid': 'prskid1000', 'exp': '1000' }, { 'userid': 'devil2021', 'exp': '1000' }]
         }
     }
 
@@ -33,18 +36,55 @@ class Index extends React.Component
               </ul>
               <ul class="navbar-nav">
                 <li class="nav-item">
+                  <center><a class="navbar-brand fa fa-fw fa-book big-icon" href="/"></a></center>
+                  <p class="h6 text-warning">Posts</p>
+                </li>
+                <li class="nav-item">
+                  <center><a class="navbar-brand fa fa-fw fa-pencil big-icon" href="/"></a></center>
+                  <p class="h6 text-warning">Create</p>
+                </li>
+                <li class="nav-item">
                   <center><a class="navbar-brand fa fa-fw fa-user big-icon" href="/"></a></center>
                   <p class="h6 text-warning">{this.state.user}</p>
                 </li>
               </ul>
             </nav>
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+              <strong>{this.state.alert}</strong>
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <br></br>
             <div class="row">
               <div class="col-10 bg-warning">
-                <center><p>1 of 2</p></center> 
+                <center><p class="bg-dark col-6 h3 text-white font-weight-bolder">Top Posts!</p></center> 
+                <center>
+                    {this.state.posts.map((post, index) => (
+                      <div class="row col-11 mt-2">
+                        <div class="card col-12">
+                          <div class="card-body">
+                            <h5 class="card-title overflow-auto text-danger">{post.question}</h5>
+                            <p class="card-text overflow-auto">{post.desciption}</p>
+                            <a href="#" class="btn btn-dark">Full View</a>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                </center>
               </div>
-              <div class="col-2 bg-danger">
-                <center><p>2 of 2</p></center> 
+              <div class="col-2 bg-muted">
+                <center><p class="bg-dark col text-white font-weight-bolder">Top Contributors!</p></center>
+                  {this.state.contributors.map((user, index) => (
+                    <span class="h4 badge badge-danger">
+                      {user.userid}
+                      <span class="h6 badge badge-success">
+                        {user.exp}
+                      </span>
+                    </span>
+                  ))}
               </div>
+              <a href="#" class="btn btn-dark col-10 mt-2 mb-5">View More</a>
             </div>
           </div>
         );
